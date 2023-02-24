f3dcee28ead7ef0486bf559642a6deec2047c476
nafas-deni <deni@nafas.co.id>
2023-02-24 15:00:51 +0700

    init commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=8" />
+  <meta http-equiv="no-cache"> 
+  <title>Nafas- File Manager</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <link href="assets/css/app.css" rel="stylesheet">
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/css/font.css" rel="stylesheet">
+  <link href="assets/css/demo.css" rel="stylesheet">
+</head>
+<style>
+.container-inside{
+	margin-top:15vh;
+	margin-left:200px;
+	margin-right:200px;
+}
+.rounded{
+	border-radius: 75px;
+	border: 10px solid black;
+	width: 150px;
+	height: 150px;
+	text-align: center;
+	vertical-align: middle;
+	padding-top:20px;
+}
+.rounded:hover{
+	border-radius: 75px;
+	border: 10px solid black;
+	background-color:#4f81bd;
+	width: 150px;
+	height: 150px;
+	text-align: center;
+	vertical-align: middle;
+	padding-top:20px;
+}
+.text-icon{
+	font-size:24px;
+	font-weight:bold;
+}
+.text-icon-md{
+	font-size:20px;
+	font-weight:bold;
+}
+.text-icon-sm{
+	font-size:18px;
+	font-weight:bold;
+}
+a{
+	color:black;
+}
+
+</style>
+<body>
+<div class="container">
+<div class="row row-sm text-center">
+<!-- <div class="container-inside"> -->
+			<br/>
+			<br/>
+			<br/>
+			
+			<div class="col-md-4 col-sm-6">
+					<a href="./file/nafas_caz_tablet.apk" target="_blank">
+						<img src="./images/nafas_caz_tablet.png" width="150" height="150">
+						<br/>
+						<h4><b>Nafas Caz Tablet</b></h4>
+					</a>
+			</div>
+
+			<div class="col-md-4 col-sm-6">
+					<a href="./file/nafas_beta.apk" target="_blank">
+						<img src="./images/nafas_beta.png" width="150" height="150">
+						<br/>
+						<h4><b>Nafas Beta</b></h4>
+					</a>
+			</div>
+			
+			
+			<br/>
+			<br/>
+			<br/>
+<!-- </div> -->
+</div>
+</div>
+</body>
+</html>
